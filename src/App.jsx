08cbac75a7db538fe25b9fd02f6620938780b4ac
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-26 08:36:21 -0500

    Added all the created components to the App class

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import { Home } from '../src/Home/Home.jsx'
+import { Account } from '../src/Accounts/Account.jsx'
+import { NotFound } from '../src/NotFound/NotFound.jsx'
+import { Order } from '../src/Orders/Order.jsx'
+import { Signin } from '../src/Signin/Signin.jsx'
+import './App.css';
+
 
 function App() {
 
   return (
     <div className='bg-red-800'>
-      hello world
+      <Home />
+      <Account />
+      <NotFound />
+      <Order />
+      <Signin />
     </div>
   )
 }
+
+
 
 export default App
