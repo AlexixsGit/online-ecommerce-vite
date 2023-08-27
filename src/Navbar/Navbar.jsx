69c949fd6e592389eecba6b64fecc1d2260687fc
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-27 10:10:32 -0500

    Added underlined style to the active tab in the navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,54 +1,66 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const activeStyle = 'underline';
     return (
         <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
             <ul className="flex items-center gap-3">
                 <li className="font-semibold text-lg">
-                    <NavLink to='/'>
+                    <NavLink to='/'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Shopping
                     </NavLink>
+
                 </li>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink to='/all'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/clothes'>
+                    <NavLink to='/clothes'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/electronics'>
+                    <NavLink to='/electronics'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Electronics
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/furniture'>
+                    <NavLink to='/furniture'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Furniture
                     </NavLink>
                 </li>
+                <li>
+                    <NavLink to='/others'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
+                        Others
+                    </NavLink>
+                </li>
             </ul>
-            
+
             <ul className="flex items-center gap-3">
                 <li>
-                    <NavLink to='/order'>
+                    <NavLink to='/order'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Orders
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/account'>
+                    <NavLink to='/account'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Accounts
                     </NavLink>
                 </li>
+
                 <li>
-                    <NavLink to='/'>
-                        Others
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in'>
+                    <NavLink to='/sign-in'
+                        className={({ isActive }) => isActive ? activeStyle : undefined}>
                         Sigin
                     </NavLink>
                 </li>
