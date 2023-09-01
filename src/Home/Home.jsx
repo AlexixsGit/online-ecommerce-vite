104a9ec787e463a9ea7a94b6469189544b614b17
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2023-08-31 20:30:12 -0500

    Consume an external api

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import { Layout } from "../Layout/Layout";
 
 export function Home() {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(respose => setItems(respose))
+    }, [])
+
     return (
         <Layout className="bg-red-100">
             Home
-            <Card />
+            {
+                items?.map((item) => (
+                    <Card key={item.id} data={item} />
+                ))
+            }
         </Layout>
     );
 }
